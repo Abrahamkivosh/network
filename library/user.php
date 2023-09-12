c5bab0c274bf93bb859188663b8170a5286a89cc
wayapay <abraham@wayapay.com>
2023-09-12 13:53:40 +0300

    added invoice instance into user model

--- library/user.php
+++ library/user.php
@@ -13,9 +13,6 @@ if (!defined('CONFIG_READ_INCLUDED')) {
     define('CONFIG_READ_INCLUDED', true);
 }
 
-
-
-
 class User {
     public $configValues;
     public $userInstance;
@@ -279,13 +276,386 @@ class User {
         }
     }
 
+    /**
+     * Get user all invoices
+     * @return mixed
+     */
+    public function getUserInvoices()
+    {
+     
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $orderBy = 'a.id';
+        $orderType = 'DESC';
+        $rowsPerPage = 10;
+        $offset = 0;
+
+        $sql_WHERE = " WHERE b.username = '$userInstance' ";
+        
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+        $query = "SELECT a.id, a.date, a.status_id, a.type_id, b.contactperson, b.username, ".
+        " c.value AS status, COALESCE(e2.totalpayed, 0) as totalpayed, COALESCE(d2.totalbilled, 0) as totalbilled ".
+        " FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICE']." AS a".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO']." AS b ON (a.user_id = b.id) ".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS']." AS c ON (a.status_id = c.id) ".
+        " LEFT JOIN (SELECT SUM(d.amount + d.tax_amount) ".
+                " as totalbilled, invoice_id FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS']." AS d ".
+        " GROUP BY d.invoice_id) AS d2 ON (d2.invoice_id = a.id) ".
+        " LEFT JOIN (SELECT SUM(e.amount) as totalpayed, invoice_id FROM ". 
+        $configValues['CONFIG_DB_TBL_DALOPAYMENTS']." AS e GROUP BY e.invoice_id) AS e2 ON (e2.invoice_id = a.id) ".
+        $sql_WHERE.
+        " GROUP BY a.id ".
+        " ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+        $result = $this->dbSocket->query($query);
+
+        if ($result->numRows()  > 0) {
+            // get all rows
+            $rows = [] ;
+            while ($row = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $rows[] = $row;
+            }
+            return $rows;
+            
+        } else {
+            return false;
+        }
+
+
+
+    }
+
+    /**
+     * Get user invoice by id
+     * @param $invoiceId
+     * @return mixed
+     */
+    public function getUserInvoiceById($invoiceId)
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $orderBy = 'a.id';
+        $orderType = 'DESC';
+        $rowsPerPage = 10;
+        $offset = 0;
+
+        $sql_WHERE = " WHERE b.username = '$userInstance' AND a.id = '$invoiceId' ";
+
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+
+        $query = "SELECT a.id, a.date, a.status_id, a.type_id, b.contactperson, b.username, ".
+        " c.value AS status, COALESCE(e2.totalpayed, 0) as totalpayed, COALESCE(d2.totalbilled, 0) as totalbilled ".
+        " FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICE']." AS a".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO']." AS b ON (a.user_id = b.id) ".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS']." AS c ON (a.status_id = c.id) ".
+        " LEFT JOIN (SELECT SUM(d.amount + d.tax_amount) ".
+                " as totalbilled, invoice_id FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS']." AS d ".
+        " GROUP BY d.invoice_id) AS d2 ON (d2.invoice_id = a.id) ".
+        " LEFT JOIN (SELECT SUM(e.amount) as totalpayed, invoice_id FROM ".
+        $configValues['CONFIG_DB_TBL_DALOPAYMENTS']." AS e GROUP BY e.invoice_id) AS e2 ON (e2.invoice_id = a.id) ".
+        $sql_WHERE.
+        " GROUP BY a.id ".
+        " ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+        $result = $this->dbSocket->query($query);
+
+        if ($result->numRows()  > 0) {
+            // get all rows
+            $rows = [] ;
+            while ($row = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $rows[] = $row;
+            }
+            return $rows;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Get user latest invoice by status
+     * @param $status
+     */
+    public function getUserLatestInvoiceByStatus(string $status)
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $orderBy = 'a.id';
+        $orderType = 'DESC';
+        $rowsPerPage = 10;
+        $offset = 0;
+        $status = strtolower($status);
+        
+        $sql_WHERE = " WHERE b.username = '$userInstance'  AND c.value = '$status' ";
+
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+
+        $query = "SELECT a.id, a.date, a.status_id, a.type_id, b.contactperson, b.username, ".
+        " c.value AS status, COALESCE(e2.totalpayed, 0) as totalpayed, COALESCE(d2.totalbilled, 0) as totalbilled ".
+        " FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICE']." AS a".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO']." AS b ON (a.user_id = b.id) ".
+        " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS']." AS c ON (a.status_id = c.id) ".
+        " LEFT JOIN (SELECT SUM(d.amount + d.tax_amount) ".
+                " as totalbilled, invoice_id FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS']." AS d ".
+        " GROUP BY d.invoice_id) AS d2 ON (d2.invoice_id = a.id) ".
+        " LEFT JOIN (SELECT SUM(e.amount) as totalpayed, invoice_id FROM ".
+        $configValues['CONFIG_DB_TBL_DALOPAYMENTS']." AS e GROUP BY e.invoice_id) AS e2 ON (e2.invoice_id = a.id) ".
+        $sql_WHERE.
+        " GROUP BY a.id ".
+        " ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+
+        $result = $this->dbSocket->query($query);
+
+        if ($result->numRows()  > 0) {
+            // get first row
+            $row = $result->fetchRow(DB_FETCHMODE_ASSOC);
+            return $row;
+
+        } else {
+            return false;
+        }
+
+    }
+
+
+    /**
+     * Get user invoice items
+     * @param $invoiceId
+     * @return mixed
+     */
+    public function getUserInvoiceItems($invoiceId)
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $orderBy = 'a.id';
+        $orderType = 'DESC';
+        $rowsPerPage = 10;
+        $offset = 0;
+
+        $sql_WHERE = " WHERE  a.invoice_id = '$invoiceId' ";
+
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+
+        $query = "SELECT a.id, a.invoice_id, a.amount, a.tax_amount, a.notes, a.plan_id, ".
+         " b.planName, b.planCost, b.planTimeBank ".
+            " FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS']." AS a".
+            " INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOBILLINGPLANS']." AS b ON (a.plan_id = b.id) ".
+            $sql_WHERE
+            ." ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+        $result = $this->dbSocket->query($query);
+
+        if ($result->numRows()  > 0) {
+            // get all rows
+            $rows = [] ;
+            while ($row = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $rows[] = $row;
+            }
+            return $rows;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Get user invoice payments
+     * @param $invoiceId
+     */
+    public function getUserInvoicePayments($invoiceId)
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $orderBy = 'a.id';
+        $orderType = 'DESC';
+        $rowsPerPage = 10;
+        $offset = 0;
+
+        $sql_WHERE = " WHERE  a.invoice_id = '$invoiceId' ";
+
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+
+        $query = "SELECT a.id, a.invoice_id, a.amount, a.type_id, a.date, a.notes,".
+        "b.value as type".
+            " FROM ".$configValues['CONFIG_DB_TBL_DALOPAYMENTS']." AS a".
+            " LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOPAYMENTTYPES']." AS b ON (a.type_id = b.id) ".
+            $sql_WHERE
+            ." ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+           
+        $result = $this->dbSocket->query($query);
+
+        if ($result->numRows()  > 0) {
+            // get all rows
+            $rows = [] ;
+            while ($row = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $rows[] = $row;
+            }
+            return $rows;
+
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get user balance from invoices payments
+     * Deduct invoice items amount to get balance
+     */
+    public function getUserBalance()
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+
+        $sql_WHERE = " WHERE b.username = '$userInstance' ";
+
+        $sql = "SELECT i.id AS invoice_id, i.user_id, i.date As invoice_date, ".
+        "COALESCE(SUM(p.amount), 0) AS totalpayed, ".
+        "COALESCE(SUM(ii.amount + ii.tax_amount), 0) AS totalbilled, ".
+        "COALESCE(SUM(p.amount), 0) - COALESCE(SUM(ii.amount + ii.tax_amount), 0)  AS balance ".
+        "FROM ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICE']." AS i ".
+        "INNER JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO']." AS b ON (i.user_id = b.id) ".
+        "LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOPAYMENTS']." AS p ON (i.id = p.invoice_id) ".
+        "LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS']." AS ii ON (i.id = ii.invoice_id) ".
+        $sql_WHERE.
+        " GROUP BY i.id ".
+        " ORDER BY i.id DESC;";
+        $result = $this->dbSocket->query($sql);
+
+        if ($result->numRows()  > 0) {
+            // get all rows
+            $balance = 0 ;
+            while ($row = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $balance = $row['balance'];
+            }
+            return $balance;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Create user invoice if no invoice  with status  open exists
+     * status open is picked from invoice_type table
+     */
+    public function createUserInvoice(array $data)
+    {
+        $userInstance = $this->userInstance;
+        $configValues = $this->configValues;
+        $table_invoice = $configValues['CONFIG_DB_TBL_DALOBILLINGINVOICE'];
+        $table_invoice_items = $configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS'];
+        $table_invoice_status = $configValues['CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS'];
+        $table_invoice_type = $configValues['CONFIG_DB_TBL_DALOBILLINGINVOICETYPE'];
+        $table_userBillInfo = $configValues['CONFIG_DB_TBL_DALOUSERBILLINFO'];
+
+        $userName = $this->dbSocket->escapeSimple($userInstance);
+        $userBillInfo = $this->getUserBillInfo();
+        $userId = $userBillInfo['id'];
+
+        $invoice_type = $data['invoice_type'];
+        $invoice_status = $data['invoice_status'];
+        $invoice_date = $data['invoice_date'];
+        $invoice_notes = $data['invoice_notes'];
+        $invoice_plan_id = $data['invoice_plan_id'];
+        
+        
+        // check if invoice with status open exists
+        $invoices = $this->getUserInvoices();
+        $invoice_exists = false;
+        foreach ($invoices as $invoice) {
+           $status = strtolower($invoice['status']);
+              if ($status == 'open') {
+                $invoice_exists = true;
+              }
+        }
+
+        if (!$invoice_exists) {
+            // create invoice
+            $sql = "INSERT INTO $table_invoice (user_id, date, status_id, type_id, notes) VALUES ".
+            "('$userId', '$invoice_date', '$invoice_status', '$invoice_type', '$invoice_notes')";
+            $result = $this->dbSocket->query($sql);
+            if ($result) {
+                // get invoice id
+                $invoice_id = $this->dbSocket->lastInsertID();
+                // return $invoice_id;
+                return $invoice_id;
+                
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Create user invoice items
+     * @param array $data
+     * @return mixed
+     */
+    public function createUserInvoiceItems(int $invoice_id, array $data)
+    {
+        $plan_id = $data['plan_id'];
+        $amount = $data['amount'];
+        $tax_amount = $data['tax_amount'];
+        $notes = $data['notes'];
+        $total = $amount + $tax_amount;
+
+        $configValues = $this->configValues;
+        $table_invoice_items = $configValues['CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS'];
+
+        $sql = "INSERT INTO $table_invoice_items (invoice_id, plan_id, amount, tax_amount, notes) VALUES ".
+        "('$invoice_id', '$plan_id', '$amount', '$tax_amount', '$notes')";
+        $result = $this->dbSocket->query($sql);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Create user invoice payments
+     * @param array $data
+     * @return mixed
+     */
+    public function createUserInvoicePayments(int $invoice_id, array $data)
+    {
+        $amount = $data['amount'];
+        $type_id = $data['type_id'];
+        $date = isset($data['date']) ? $data['date'] : date('Y-m-d H:i:s');
+        $notes = isset ($data['notes']) ? $data['notes'] : '';
+
+        $configValues = $this->configValues;
+        $table_invoice_payments = $configValues['CONFIG_DB_TBL_DALOPAYMENTS'];
+
+        $sql = "INSERT INTO $table_invoice_payments (invoice_id, amount, type_id, date, notes) VALUES ".
+        "('$invoice_id', '$amount', '$type_id', '$date', '$notes')";
+        $result = $this->dbSocket->query($sql);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
 
 }
 
 // test the class
-$user = new User($dbSocket, $configValues);
-$user->setUserInstance('kivosh');
-$billingInfo = $user->getUserBillInfo();
+// $user = new User($dbSocket, $configValues);
+// $user->setConfigValues($configValues);
+// $user->setUserInstance('kivosh');
+// $userExists = $user->userExists();
+// $getUserInvoices = $user->getUserLatestInvoiceByStatus('open');
+
+// echo '<pre>';
+// print_r($getUserInvoices);
+// echo '</pre>';
 
 
