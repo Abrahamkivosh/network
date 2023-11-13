e0de6ae7ccf9425552b2fcf26e050aa6cad28d62
wayapay <abraham@wayapay.com>
2023-11-13 11:00:43 +0300

    show errors

--- library/daloradius.conf.php
+++ library/daloradius.conf.php
@@ -1,4 +1,12 @@
 <?php
+
+// display errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
 // Path: library/daloradius.conf.php
 require_once __DIR__ . '/../vendor/autoload.php';
 
